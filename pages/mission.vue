c52d53f0f14adda8bf3849a276cb583e43fee5b8
Ben Layer <benlayer291@gmail.com>
2018-05-14 00:14:30 +0100

    Update pages with hero and split content

--- pages/mission.vue
+++ pages/mission.vue
@@ -1,10 +1,21 @@
 <template>
-  <main></main>
+  <main>
+  
+    <!-- Hero -->
+    <Hero 
+      p-text="<h1 class='t-lede'>Sipple was founded to combat the environmental damage caused by the bottled water industry.</h1>"
+    />
+  
+  </main>
 </template>
 
 <script>
+import Hero from '@/components/Hero'
+
 export default {
-  
+  components: {
+    Hero,
+  }
 }
 </script>
 
---
+++ pages/story.vue
@@ -0,0 +1,46 @@
+<template>
+  <main>
+  
+    <!-- Hero -->
+    <Hero 
+      p-text="<h1 class='t-lede'>The future's not disposable</h1>"
+    />
+
+    <!-- Split -->
+    <Split
+      p-image="6.jpg"
+      p-color="#53565a"
+      p-background-color="#fff"
+      p-text="
+        <p>Forty years ago, plastic was a new wonder material. Lightweight, durable and cheap. We all got so excited we threw parties about tupperware.</p>
+        <p>Common sense at the time said selling water in a plastic bottle was a terrible idea. But someone sensed a buck to be made and did it anyway.</p>
+      "
+      :p-reverse="true"
+    />
+
+    <!-- Split -->
+    <Split
+      p-image="7.jpg"
+      p-color="#53565a"
+      p-background-color="#fff"
+      p-text="
+        <p>Today there's a stirring around disposable plastics. Their impact is under scrutiny. It's being discussed on TV, in the news and in government. Uncomfortable questions are being asked. People haven't been given much choice.</p>
+        <p>Until now.</p>
+      "
+      :p-reverse="false"
+    />
+
+  </main>
+</template>
+
+<script>
+import Hero from '@/components/Hero'
+import Split from '@/components/Split'
+
+export default {
+  components: {
+    Hero,
+    Split,
+  },
+}
+</script>
