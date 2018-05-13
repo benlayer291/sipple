0e444bc208b7c055e76ac02b6a1c00be9f673dd9
Ben Layer <benlayer291@gmail.com>
2018-05-13 15:01:58 +0100

    Add props to signoff component

--- components/Signoff.vue
+++ components/Signoff.vue
@@ -4,7 +4,40 @@
 
 <script>
 export default {
-
+  props: {
+    pBackgroundColor: {
+      type: String,
+      required: false,
+      default: '#503bd2',
+    },
+    pCta:{
+      type: Object,
+      required: false
+    },
+    pImage: {
+      type: String,
+      required: false,
+    },
+    pText: {
+      type: String,
+      required: false,
+    },
+    pTitle: {
+      type: String,
+      required: false,
+    },
+  },
+  data() {
+    return {
+      backgroundStyle: {
+        backgroundColor: this.pBackgroundColor,
+      },
+      cta: this.pCta,
+      image: this.pImage,
+      text: this.pText,
+      title: this.pTitle,
+    }
+  },
 }
 </script>
 
